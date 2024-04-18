7567e0ada492cfa19a1a257a41237cee85b7e055
Diary <raharimanantenadiary@gmail.com>
2024-04-18 11:21:01 +0300

    ajout fonction Services Matieres

--- src/app/Services/matieres.service.ts
+++ src/app/Services/matieres.service.ts
@@ -8,5 +8,19 @@ export class MatieresService {
 
   matieres : Matieres[] = []
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8010/api/matieres';
+
+  constructor(private http: HttpClient) { }
+
+  getMatiereById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${id}`);
+  }
+
+  postMatiere(matiere: Matieres): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/ajouter`, matiere);
+  }
+
+  updateMatiere(matiere: Matieres): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/modifier/${matiere._id}`, matiere);
+  }
 }
