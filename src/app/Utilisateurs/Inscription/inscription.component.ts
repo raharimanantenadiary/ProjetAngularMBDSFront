fb697127be4ed4e9af348236dccdca6e7a76857d
Hasina1234 <89484785+Hasina1234@users.noreply.github.com>
2024-05-30 10:25:25 +0300

    inscription

--- src/app/Utilisateurs/Inscription/inscription.component.ts
+++ src/app/Utilisateurs/Inscription/inscription.component.ts
@@ -2,26 +2,36 @@ import { Component } from '@angular/core';
 import { UtilisateursService } from '../../Services/utilisateurs.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
 import {
   FormControl,
   Validators,
   FormsModule,
   ReactiveFormsModule,
+  NgForm
 } from '@angular/forms';
 
 @Component({
   selector: 'app-inscription',
   standalone: true,
-  imports: [MatProgressSpinnerModule,CommonModule,MatCardModule,MatFormFieldModule, MatInputModule,MatIconModule,FormsModule,ReactiveFormsModule,MatButtonModule],
+  imports: [
+    MatProgressSpinnerModule,
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule
+  ],
   templateUrl: './inscription.component.html',
-  styleUrl: './inscription.component.css'
+  styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent {
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
@@ -31,10 +41,10 @@ export class InscriptionComponent {
   isLoading: boolean = false;
   errorMessage = '';
 
-  constructor(private utilisateursService: UtilisateursService,private router: Router) { }
+  constructor(private utilisateursService: UtilisateursService, private router: Router) {}
 
   togglePasswordVisibility() {
-      this.isPasswordVisible = !this.isPasswordVisible;
+    this.isPasswordVisible = !this.isPasswordVisible;
   }
 
   isPasswordNotEmpty(): boolean {
@@ -42,15 +52,19 @@ export class InscriptionComponent {
     return typeof passwordValue === 'string' && passwordValue.length > 0;
   }
 
-  inscrireUtilisateur() {
+  inscrireUtilisateur(form: NgForm) {
     this.isLoading = true;
     const email = this.emailFormControl.value ?? '';
     const motDePasse = this.motDePasseFormControl.value ?? '';
     const nom = this.nomFormControl.value ?? '';
-    this.utilisateursService.sInscrire(nom, email, motDePasse, "","1").subscribe(
+    this.utilisateursService.sInscrire(nom, email, motDePasse, "", "1").subscribe(
       (response: any) => {
         this.isLoading = false;
-        this.errorMessage = 'Utilisateur inscrit avec succès';
+        this.errorMessage = 'Utilisateur inscrit avec succès, retourner vers la page login pour se connecter.';
+        form.resetForm();
+        this.emailFormControl.reset();
+        this.nomFormControl.reset();
+        this.motDePasseFormControl.reset();
       },
       (error) => {
         this.isLoading = false;
@@ -60,6 +74,6 @@ export class InscriptionComponent {
   }
 
   seRedirigerVersLogin() {
-  this.router.navigate(['/login']);
-}
+    this.router.navigate(['/login']);
+  }
 }
